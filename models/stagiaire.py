a9e726b780b64fbcb2d6ba25fe8b5260d706d92e
KIMA Thierry Tewende2 <kimatewende@gmail.com>
2022-11-03 15:09:30 +0000

    ajout actions

--- models/stagiaire.py
+++ models/stagiaire.py
@@ -13,8 +13,11 @@ class Stagiaire(models.Model):
     x_responsable_id=fields.Many2one("responsable",string="Responsable")
     taches_ids = fields.Many2many('taf','stagiaire_taf', 'stagiaire_id', 'taf_id', string="Tâches")
     image = fields.Binary()
-    progress_state = fields.Selection([('attente', 'En attente'),('valide', 'Valider')], string='State',default='attente')
+    progress_state = fields.Selection([('rejet', 'Rejeter'),('attente', 'En attente'),('valide', 'Valider')], string='State',default='attente')
     
     def set_valide(self):       
-        self.write({'progress_state': 'valide'})  
+        self.write({'progress_state': 'valide'})
         
+    def set_rejet(self):
+        self.write({'progress_state': 'rejet'})   
+
